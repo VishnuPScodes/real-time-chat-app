e161b880a274fee9b5abdfdfb85eee6d7b571156
vishnups0305 <95961812+VishnuPScodes@users.noreply.github.com>
2023-09-18 15:42:52 +0530

    env configured

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log('Listening to the port', PORT);
+});
