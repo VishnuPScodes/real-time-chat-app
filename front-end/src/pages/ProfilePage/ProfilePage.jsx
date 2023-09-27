c7b9b2b87152a1664275d2830e8e9853a55de40b
vishnups0305 <95961812+VishnuPScodes@users.noreply.github.com>
2023-09-27 13:24:40 +0530

    adds profile page

--- front-end/src/pages/ProfilePage/ProfilePage.jsx
+++ front-end/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import './profile.css'
+
 function ProfilePage() {
     return (
         <div className='profile-container'>
@@ -10,6 +11,15 @@ function ProfilePage() {
             </div>
             <div className="participants_name">
                 234 Participants
+            </div>
+            <div className='single-chat' >
+                <div className='image-head'>
+                    <img height={'100%'} width={'100%'} style={{ borderRadius: "50%" }} src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt="" />
+                </div>
+                <div className='participant-box'>
+                    <div className='user-name'> Vishnu </div>
+                    <div className="admin">Admin</div>
+                </div>
             </div>
         </div>
     )
