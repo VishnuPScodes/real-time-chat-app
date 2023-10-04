310f1f081b274ea87487f51f4b0fd500540012fa
vishnups0305 <95961812+VishnuPScodes@users.noreply.github.com>
2023-10-04 12:59:19 +0530

    adds validator

--- backend/routes/reg.routes.js
+++ backend/routes/reg.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { registerUser, allUsers } from '../controllers/reg.controller.js';
+import { regValidator } from '../validators/reg.validator.js';
 
 
 const router = express.Router();
 
-router.post('/', registerUser);
+router.post('/', regValidator, registerUser);
 router.get('/', allUsers);
 export default router
--- backend/validators/reg.validator.js
+++ backend/validators/reg.validator.js
@@ -1,6 +1,23 @@
-import { body } from 'express-validator'
+import { body, validationResult } from 'express-validator'
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
 
-export const regValidator = () => {
-    return body('email').notEmpty()
+export const validateRequest = [xss(), mongoSanitize(), async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(422).json({
+            error: errors.array()[0].msg,
+        });
+    }
+    else {
+        next();
+    }
+}];
 
-}
+export const regValidator = [
+    body('email').notEmpty(),
+    ...validateRequest
+]
+
+
+
